4243fb915ec8ab00f0e4fd2cc54bb7fe33dd3e8f
Ryan Durk <rpdurk@gmail.com>
2020-11-25 21:16:56 -0800

    updated UserSchema

---
+++ model/UserSchema.js
@@ -0,0 +1,27 @@
+const UserSchema = new Schema({
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: [true, 'Password is required'],
+    },
+    firstName: {
+        type: String,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+    },
+    role: {
+        type: String,
+        required: [true, 'You must select a role'],
+    },
+  }, {
+    timestamps: true,
+  });
