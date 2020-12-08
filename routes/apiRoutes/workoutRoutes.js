182338a3a6c967ab88918fc5b27c9599dc71a2c0
Ryan Durk <rpdurk@gmail.com>
2020-12-07 19:38:03 -0800

    updated workout routes

--- routes/apiRoutes/workoutRoutes.js
+++ routes/apiRoutes/workoutRoutes.js
@@ -1,33 +1,20 @@
 const router = require('express').Router();
-const {
-  returnAllWorkoutsByUserId,
-  returnWorkoutsById,
-  returnWorkoutsByWorkoutName,
-  addWorkout,
-  deleteWorkouts,
-} = require('../../controllers/workoutController');
 
-// GET Routes
-// GET -> /:userId          -> Returns All workouts by User
-// GET -> /:workoutId     -> Returns all workouts by workout ID
-// GET -> /:workoutName    -> Returns all workout by a specific name
-// Post -> /addWorkout     -> Add workout object into the database
-// DELETE -> /deleteWorkout    -> Deletes workout object from the database
+const workoutController = require('../../controllers/workoutController');
 
+// const authMiddleware = require('../../../middlewares/authorizationMiddleware');
+// router.use(authMiddleware);
 
-// Get workouts by User ID
-router.get('/user/:userId', returnAllWorkoutsByUserId);
-
-// Get workout by workout ID
-router.get('/id/:workoutId', returnWorkoutsById);
-
-// Get workout by workoutName
-router.get('/name/:workoutName', returnWorkoutsByWorkoutName);
-
-// Add a workout
-router.post('/addWorkout', addWorkout);
-
-// Delete a workout
-router.delete('/:workoutId', deleteWorkouts);
-
+// at api/users/:id
+router.route('/:id')
+  // creates a workout 
+  .post(workoutController.createWorkouts)
+  // Gets a all workouts
+  .get(workoutController.getAllWorkouts)
+  // Gets all workouts by user Id
+  .get(workoutController.getAllWorkoutsByUserId)
+  // Updates a workout by workout Id
+  .patch(workoutController.updateWorkoutById)
+  // Deletes a workout by workout Id
+  .delete(workoutController.deleteWorkout)
 module.exports = router;
