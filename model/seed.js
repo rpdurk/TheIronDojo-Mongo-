4561cef04d8bb4de922c29cd6c9d8f302d6669d1
Ryan Durk <rpdurk@gmail.com>
2020-12-10 20:45:31 -0800

    Updated Seed

--- model/seed.js
+++ model/seed.js
@@ -1,128 +1,92 @@
 require('dotenv').config();
-let db = require("../models");
-const { connectDB } = require("../config/connection");
+const mongoose = require('mongoose');
+let db = require("mongoose");
+const {
+  connectDB
+} = require("../config/connection");
 connectDB();
-
-let workoutSeed = [
-  {
-    day: new Date().setDate(new Date().getDate()-10),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bicep Curl",
-        duration: 20,
-        weight: 100,
-        reps: 10,
-        sets: 4
-      }
-    ]
+let workoutSeed = [{
+    workoutName: "Monday Lift",
+    exercise: "Bench",
+    date: "12/05/2020",
+    id:'1'
   },
   {
-    day: new Date().setDate(new Date().getDate()-9),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Lateral Pull",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-8),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Push Press",
-        duration: 25,
-        weight: 185,
-        reps: 8,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-7),
-    exercises: [
-      {
-        type: "cardio",
-        name: "Running",
-        duration: 25,
-        distance: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-6),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 285,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-5),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date(new Date().setDate(new Date().getDate() - 4)),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Quad Press",
-        duration: 30,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date(new Date().setDate(new Date().getDate() - 3)),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date(new Date().setDate(new Date().getDate() - 2)),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Military Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
+    workoutName: "Tuesday Lift",
+    exercise: "PushUp",
+    date: "12/05/2020",
   }
 ];
-
+let exerciseSeed = [{
+  exerciseName: "Bench",
+  sets: 6,
+  repetitions: 8,
+  weight:135,
+  date: "12/09/2020",
+  // user_Id:
+},
+{
+  exerciseName: "PushUp",
+  sets: 6,
+  repetitions: 8,
+  weight:45,
+  date: "12/09/2020",
+  // user_Id:
+}
+];
+let biometricSeed = [{
+  weight: 120,
+  height: 70,
+  date: "12/10/2020",
+  // user_Id:
+},
+{
+  weight: 150,
+  height: 70,
+  date: "12/15/2020",
+  // user_Id:
+}
+];
+let userSeed = [
+  {
+    username:"test",
+    password:"123",
+    firstName:"one",
+    lastName:"two",
+  }
+];
 db.Workout.deleteMany({})
   .then(() => db.Workout.collection.insertMany(workoutSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+db.Exercise.deleteMany({})
+  .then(() => db.Exercise.collection.insertMany(exerciseSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+  db.Biometric.deleteMany({})
+  .then(() => db.Biometric.collection.insertMany(biometricSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+  db.User.deleteMany({})
+  .then(() => db.User.collection.insertMany(userSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
