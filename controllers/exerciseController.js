17483103578db661469b47a1735dad424863dff8
Ryan Durk <rpdurk@gmail.com>
2020-12-20 18:40:26 -0800

    updated route/controller to pull last 7 days of exercises

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -37,6 +37,24 @@ module.exports = {
       res.status(500).json(e);
     }
   },
+  getAllExercisesInLastSevenDaysByUserId: async (req, res) => {
+    const id = req.user._id;
+    console.log(id);
+    try {
+      res.json(await Exercise.find({ 
+        user_id: id,
+        createdAt:
+          { '$lte': new Date(), '$gte': new Date(new Date() - 7 * 60 * 60 * 24 * 1000) }
+      }));
+      // console.log(res);
+      // console.log(new Date());
+      // console.log(new Date(new Date() - 7 * 60 * 60 * 24 * 1000));
+      // res.json(await Exercise.find({ user_id: id }));
+    } catch (e) {
+      console.log('L: 40 exerciseController', e);
+      res.status(500).json(e);
+    }
+  },
   updateExercisesByExerciseId: async (req, res) => {
     try {
       const {
--- routes/apiRoutes/exerciseRoutes/exerciseRoutes.js
+++ routes/apiRoutes/exerciseRoutes/exerciseRoutes.js
@@ -17,4 +17,10 @@ router
 // Deletes a exercise by exercise Id
 // .delete(exerciseController.deleteExercise);
 
+// at api/exercise/7
+router
+  .route('/7')
+  // get all exercises in the last 7 days and sort by ID
+  .get(exerciseController.getAllExercisesInLastSevenDaysByUserId)
+
 module.exports = router;
